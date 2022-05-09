dd5a2c7b345882a142b8278b18950cd1c3d67391
Sisbaanas <97680028+Sisbaanas@users.noreply.github.com>
2022-05-09 15:31:58 +0100

    .

---
+++ src/test/java/basic_auth_jwt/BasicAuthJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package basic_auth_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicAuthJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
